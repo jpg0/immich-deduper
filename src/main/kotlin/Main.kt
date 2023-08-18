6699485cf47ffd8ae13790021e6117be09bca52e
Jonathan Gilbert <jpg@trillica.com>
2023-08-18 17:27:38 +1000

    now works for basic api call

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,63 @@
 package com.trillica
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.trillica.client.ApiKeyClient
+import com.trillica.client.SearchClient
+import com.trillica.models.AssetResponseDto
+import okhttp3.Headers
+import okhttp3.OkHttpClient
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    val apiKey = args[0]
+
+    val ctx = Ctx(
+        ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .registerModule(JavaTimeModule()),
+        "http://192.168.1.15:2283/api",
+        OkHttpClient().newBuilder().addInterceptor {
+            val newHeaders = Headers.Builder()
+                .addAll(it.request().headers)
+                .add("x-api-key", apiKey).build()
+            val request = it.request().newBuilder().headers(newHeaders).build()
+            it.proceed(request)
+        }.build())
+
+    val dedupers = listOf(MyOrbthumbs())
+
+    dedupers.forEach { deduper ->
+        deduper.candidates(ctx).forEach { candidate ->
+            if(deduper.verify(candidate)) {
+                println("Delete $candidate")
+            }
+        }
+    }
+
+}
+
+class Ctx(private val objectMapper: ObjectMapper,
+          private val baseUrl: String,
+          private val client: OkHttpClient
+) {
+    val searchClient: SearchClient get() = SearchClient(objectMapper, baseUrl, client)
+}
+
+typealias Candidate = AssetResponseDto
+
+interface Deduper {
+    fun candidates(ctx: Ctx) : List<Candidate>
+    fun verify(c: Candidate) : Boolean
+}
+
+class MyOrbthumbs() : Deduper {
+    override fun candidates(ctx: Ctx): List<Candidate> {
+        return ctx.searchClient.search(q = "my-orbthumb").data?.assets?.items!!
+    }
+
+    override fun verify(c: Candidate): Boolean {
+        return true
+    }
 }
